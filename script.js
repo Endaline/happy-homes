b559c72d627989ccd6129507b9ce67720cd08103
Endaline <endalineihuoma7@gmail.com>
2024-04-19 12:23:04 +0100

    update on footer and responsivness on services and about page

--- script.js
+++ script.js
@@ -1,22 +1,3 @@
-const prev = document.getElementById("prev-btn");
-const next = document.getElementById("next-btn");
-const list = document.getElementById("item-list");
-
-const itemWidth = 150;
-const padding = 10;
-
-prev.addEventListener("click", () => {
-  setTimeout(() => {
-    list.scrollLeft -= itemWidth + padding;
-  }, 1000); // Delay scrolling by 1 second (1000 milliseconds)
-});
-
-next.addEventListener("click", () => {
-  setTimeout(() => {
-    list.scrollLeft += itemWidth + padding;
-  }, 350); // Delay scrolling by 1 second (1000 milliseconds)
-});
-
 var navLinks = document.getElementById("navLinks");
 function showMenu() {
   navLinks.style.right = "0";
